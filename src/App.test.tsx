b354fea99d86f1ed47b6fb1bc35a5d1a954a364c
chanchobrujo <85182515+chanchobrujo@users.noreply.github.com>
2024-11-21 15:22:42 -0500

    REFACTOR: Update to typescript.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,9 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import InitForm from './InitForm';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<InitForm />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/InitForm.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function InitForm() {
+  return (
+    <div style={{width: '20%'}}>
+      <div style={{backgroundColor: '#ee2a2f', textAlign: 'center', padding: '.5%'}}>
+        <h1 style={{color:'white'}}>
+          Ingresar numero de seguimiento y codigo.
+        </h1>
+      </div>
+      <div style={{backgroundColor: 'white'}}>
+        Body
+      </div>
+      <div style={{backgroundColor: '#ee2a2f'}}>
+        Footers buttons
+      </div>
+    </div>
+  );
+}
+
+export default InitForm;
