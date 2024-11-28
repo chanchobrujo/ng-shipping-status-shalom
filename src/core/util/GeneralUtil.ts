7806b6353ddcd89b1b8cb5410e2178c773c6e2cc
chanchobrujo <85182515+chanchobrujo@users.noreply.github.com>
2024-11-28 15:01:38 -0500

    FIX: Fixed date into table.

--- src/core/util/GeneralUtil.ts
+++ src/core/util/GeneralUtil.ts
@@ -1,5 +1,10 @@
 export const formatDate = (date: Date) => {
   const now = new Date(date);
   const hour = now.getHours() + ':' + now.getMinutes();
-  return now.getFullYear() + '-' + now.getMonth() + '-' + now.getDay() + ' ' + hour;
+  const month = addZeroInTime(now.getMonth() + 1);
+  return now.getFullYear() + '-' + month + '-' + addZeroInTime(now.getDate()) + ' ' + hour;
+}
+
+const addZeroInTime = (value: number) => {
+  return (value > 9) ? value : "0".concat(String(value))
 }
